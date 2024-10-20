ea26ef7cb7bbcba01482f705f3c2371e7432e9f1
Sanyam <sanyamarora0550@gmail.com>
2024-10-20 16:39:16 +0530

    chore(morgan): added morgan for request logging

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as morgan from 'morgan';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(morgan('combined')); // You can use different formats like 'tiny', 'combined', etc.
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
